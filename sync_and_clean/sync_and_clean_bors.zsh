8a7e365f4a089ab8286d7b65a761e56ab39394f0
Tor Hedin Brønner <torhedinbronner@gmail.com>
2012-07-13 22:02:05 +0200

    Initial commit of bors sync cron script
    Converts and cleans oslobors data on a remote scraper and
    syncs a somewhat larger copy back, but more handy copy back.

---
+++ sync_and_clean/sync_and_clean_bors.zsh
@@ -0,0 +1,101 @@
+#!/bin/zsh
+
+# globs with no match won't cause an error
+setopt nullglob
+
+# Preferences
+REMOTEPATH="/mnt/lacie/bors"
+LOCALPATH="$HOME/docs/bors"
+DROPBOX="$HOME/Dropbox/bors"
+
+
+
+relname() {
+	echo $PWD | sed -e "s=^$REMOTEPATH=="
+}
+
+copy() {
+	echo $LOCALPATH$(relname)
+	cp -v -n --preserve=timestamps $* $LOCALPATH$(relname)
+	echo "copy done"
+}
+
+
+tar_day() {
+	local DATE=$1
+	local NAME=$(basename $PWD).$DATE.$2.tar
+	echo "\nTarring $DATE"
+	if ! [[ -f "$NAME" ]]; then
+		tar --remove-files -vcf $NAME *$DATE.$2
+	else 
+		echo "Already done $NAME"
+	fi
+}
+
+tar_days() {
+	i=-1
+	while ((i++ < 400)); do
+		local DATE=$(date -d "$i days ago" "+%F")
+		local DAY=$(date -d "$DATE" "+%a")
+		echo $i
+		if [[ $DAY != Sun && $DAY != Sat ]]; then
+			# from stackoverflow should be possible to use the nullglob
+			local FILE=$(find . -maxdepth 1 -name "*$DATE.$1" -print -quit)
+			echo $FILE
+			if test -f "$FILE"; then
+				tar_day $DATE $1
+			else
+				break 
+			fi
+			
+		fi
+	done
+}
+
+tar_gzip_copy() {
+	echo "starting copy and gzip"
+	copy *csv
+	tar_days csv
+	cd xls
+	tar_days xls
+	gzip --force --verbose *tar
+	copy *gz
+	cd ..
+}
+
+xls_to_csv_and_clean() {
+ERROR=()
+	if ! [[ -d xls ]]; then
+		mkdir xls
+	fi
+
+
+	for xls in *.xls; do
+		CSV=${xls/.xls/.csv}
+		echo $CSV
+		out=$(xls2csv $xls)
+		if  [[ 0 == $? || -e $CSV ]]; then
+			mv $xls xls
+			sed -i -e "3s/^Date.*//" -e "/^\w*$/d" $CSV
+			sleep 4
+		else
+			ERROR+=out
+		fi
+	done
+	if [[ $#ERROR -gt 0 ]]; then
+		echo "$ERROR" > xls_to_csv.log
+	fi
+}
+
+
+if cd $REMOTEPATH; then 
+
+	TODAY=$(date "+%F")
+	for i in index-intraday intraday; do
+		cd $REMOTEPATH/$i
+		xls_to_csv_and_clean
+		tar_gzip_copy
+		gzip -c *$TODAY.csv.tar > $DROPBOX/intraday.$TODAY.csv.tar.gz
+	done
+
+fi
