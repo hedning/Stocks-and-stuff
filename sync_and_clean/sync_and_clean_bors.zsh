d34fee3a7389b3ae31aad22550c8bddfb326182d
Tor Hedin Brønner <torhedinbronner@gmail.com>
2012-07-17 01:53:00 +0200

    Replaces nullglob with explicit testing
    Also removes local definitions as it seemed like
    that messed up something.

--- sync_and_clean/sync_and_clean_bors.zsh
+++ sync_and_clean/sync_and_clean_bors.zsh
@@ -1,76 +1,91 @@
 #!/bin/zsh
-
 () {
 # globs with no match won't cause an error
-setopt nullglob
+# this does weird ass shit
+unsetopt nullglob
 
 # Preferences
-local REMOTEPATH="/mnt/lacie/bors"
-local LOCALPATH="$HOME/docs/bors"
-local DROPBOX="$HOME/Dropbox/bors"
+REMOTEPATH="/mnt/lacie/bors"
+LOCALPATH="$HOME/docs/bors"
+DROPBOX="$HOME/Dropbox/bors"
 
-local TYPENAME=""
+TYPENAME=""
 
-local relname() {
+relname() {
 	echo $PWD | sed -e "s=^$REMOTEPATH=="
 }
 
-local copy() {
-	echo $LOCALPATH$(relname)
+first_match() {
+	find . -maxdepth 1 -name "$1" -print -quit
+}
+
+glob_exist() {
+	match=$(first_match "$1")
+	[[ -n "$match" ]]
+}
+
+copy() {
+	echo "Starting to copy $*[1]"
 	cp -v -n --preserve=timestamps $* $LOCALPATH$(relname)
 	echo "copy done"
 }
 
 
-local tar_day() {
-	local DATE=$1
-	local NAME=$TYPENAME.$DATE.$2.tar
+tar_day() {
+	DATE=$1
+	NAME=$TYPENAME.$DATE.$2.tar
 	echo "\nTarring $DATE"
 	if ! [[ -f "$NAME" ]]; then
-		tar --remove-files -vcf $NAME *$DATE.$2
+		tar -v --remove-files -vcf $NAME *$DATE.$2
 	else 
 		echo "Already done $NAME"
 	fi
 }
 
-local tar_days() {
-	local i=-1
-	while ((i++ < 400)); do
-		local DATE=$(date -d "$i days ago" "+%F")
-		local DAY=$(date -d "$DATE" "+%a")
-		echo $i
-		if [[ $DAY != Sun && $DAY != Sat ]]; then
-			# from stackoverflow should be possible to use the nullglob
-			local FILE=$(find . -maxdepth 1 -name "*$DATE.$1" -print -quit)
-			echo $FILE
-			if test -f "$FILE"; then
-				tar_day $DATE $1
-			else
-				break 
+tar_days() {
+	i=-1
+	while ((++i > -1)); do
+		DATE=$(date -d "$i days ago" "+%F")
+		if glob_exist "*$DATE.$1"; then
+			echo "Tarring $DATE"
+			tar_day $DATE $1
+		else
+			if ! glob_exist "*.$1"; then
+				echo "No $1 files to process"
+				break
 			fi
-			
 		fi
 	done
 }
 
-local tar_gzip_copy() {
-	echo "starting copy and gzip"
-	copy *csv
+tar_gzip_copy() {
+	echo "\nStarting copy and gzip in $PWD"
+	if glob_exist "*csv"; then
+		copy *csv
+	fi
 	tar_days csv
+	echo "Entering xls"
 	cd xls
 	tar_days xls
-	gzip --force --verbose *tar
-	copy *gz
+	if glob_exist "*.tar"; then
+		gzip --verbose *.tar
+	fi
+	if glob_exist "*gz"; then
+		copy *gz
+	fi
 	cd ..
+	echo "Done tarring and moving in $TYPENAME"
 }
 
-local xls_to_csv_and_clean() {
+xls_to_csv_and_clean() {
 	local ERROR=()
 	if ! [[ -d xls ]]; then
 		mkdir xls
 	fi
 
-	local xls
+	if ! glob_exist "*.xls"; then
+		return 
+	fi
 	for xls in *.xls; do
 		CSV=${xls/.xls/.csv}
 		echo $CSV
@@ -97,8 +112,10 @@ if cd $REMOTEPATH; then
 		cd $REMOTEPATH/$i
 		xls_to_csv_and_clean
 		tar_gzip_copy
-		gzip -c *$TODAY.csv.tar > $DROPBOX/$TYPENAME.$TODAY.csv.tar.gz
+		if [[ -f $(first_match "*$TODAY.csv.tar") ]]; then
+			gzip -c *$TODAY.csv.tar > $DROPBOX/$TYPENAME.$TODAY.csv.tar.gz
+		fi
 	done
 
 fi
-}
+} $*
