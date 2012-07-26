6e959180eae7045f5398540dc889583edba91d64
Tor Hedin Brønner <torhedinbronner@gmail.com>
2012-07-26 13:41:36 +0200

    Strips commas out of numbers of csv files.
    Should be fairly robust. I think.
    Writes to stdout at the moment

---
+++ utils/stripcomma.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from sys import argv, stdout
+import csv
+import re
+
+digits = re.compile('\d|\.')
+
+def strip(string):
+	out = ""
+	for i in string:
+		if re.match(digits, i) != None:
+			out += i
+		elif i != ',':
+			return string
+	return out
+
+
+
+def stripfi(name):
+	data = [[strip(s) for s in x ]for x in csv.reader(open(name))]
+	writer = csv.writer(stdout)
+	writer.writerows(data)
+
+for name in argv[1:]:
+	stripfi(name)
